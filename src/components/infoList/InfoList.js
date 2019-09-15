178ea46c9695d4f0144ec173eb2c962df00f7337
Gabriel Weich <gabrielweich.dev@gmail.com>
2019-09-15 01:32:33 -0300

    listar pessoas em Infolist

--- src/components/infoList/InfoList.js
+++ src/components/infoList/InfoList.js
@@ -7,26 +7,34 @@ import * as React from 'react';
 import { firestore } from '../../lib/firebase';
 import Button from 'antd/es/button';
 import CustomTag from '../Tag';
-import Constants from '../../lib/constants'
+import Constants from '../../lib/constants';
 
 class InfoList extends React.Component {
   render() {
-      console.log(this.props.disaster)
+    console.log(this.props.disaster);
     return (
       <InfiniteScroll pageStart={0} useWindow={false}>
         <List
           header={
             <div style={{ display: 'flex', overflowX: 'auto' }}>
-                {Object.entries(Constants.situations)
-                    .map(([key, value]) => <CustomTag>{value} ({this.props.disaster.situations[key]})</CustomTag>)}
-              
-
+              {Object.entries(Constants.situations).map(([key, value]) => (
+                <CustomTag>
+                  {value} ({this.props.disaster.situations[key]})
+                </CustomTag>
+              ))}
             </div>
           }
-          dataSource={this.props.disaster.peoble || []}
+          dataSource={this.props.disaster.people || []}
           renderItem={item => (
             <List.Item key={item.name}>
-              <List.Item.Meta title={<a href="">{item.name}</a>} />
+              <List.Item.Meta
+                title={
+                  <span style={{ margin: 0, padding: 0, paddingLeft: 10 }}>
+                    {item.name}
+                  </span>
+                }
+              />
+              <div>{<p style={{margin: 0, padding: 0, paddingRight: 10}}>{Constants.situations[item.situation]}</p>}</div>
             </List.Item>
           )}
         ></List>
