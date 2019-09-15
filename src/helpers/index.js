3543327bc030b957d1d4cf41f0acb82b352ecfd6
Vinícius Teixeira <vinicius.teixeira@somosphi.com>
2019-09-14 21:27:26 -0300

    user auth and notification token update

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,12 +1,12 @@
 import { messaging } from '../lib/firebase';
 
-export const requestNotificationPermission = () => {
-  messaging
-    .requestPermission()
-    .then(() => {
-      console.log('Permissão Fornecida');
-    })
-    .catch(() => {
-      console.error('Permissão para notificações negada, encerrando aplicação');
-    });
+export const requestNotificationPermission = async () => {
+  try {
+    await messaging.requestPermission();
+    const token = await messaging.getToken();
+    return token;
+  } catch (e) {
+    console.log(e); 
+    console.error('Permissão para notificações negada, encerrando aplicação');
+  }
 };
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,21 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Styles from './styles';
 import { requestNotificationPermission } from '../../helpers';
 import { Form, Icon, Input, Button, Typography } from 'antd';
-import { messaging } from '../../lib/firebase';
+import { messaging, firestore, auth } from '../../lib/firebase';
 const { Title } = Typography;
 
-
-
 export const LoginScreen = () => {
+  const [cpf, setCpf] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
 
   useEffect(() => {
-    requestNotificationPermission();
-
-    messaging.onMessage((msg) => {
+    messaging.onMessage(msg => {
       alert(`Mensagem Recebida: ${msg}`);
     });
   }, []);
+
+  const handleClick = async () => {
+    const token = await requestNotificationPermission();
+
+    if (token) {
+      auth
+        .signInWithEmailAndPassword(`${cpf}@aboborasemalerta.com`, password)
+        .then(data => {
+          updateNotificationToken(data.user.uid, token);
+        })
+        .catch(err => {
+          if (err.code === 'auth/wrong-password') {
+            alert('Senha inválida');
+          } else {
+            registerUser(token);
+          }
+        });
+    }
+  };
+
+  const registerUser = async token => {
+    try {
+      const response = auth.createUserWithEmailAndPassword(
+        `${cpf}@aboborasemalerta.com`,
+        password
+      );
+      updateNotificationToken(response.user.uid, token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateNotificationToken = async (userUid, token) => {
+    firestore
+      .collection('users')
+      .doc(userUid)
+      .set({
+        notificationToken: token,
+      }, { merge: true });
+  };
 
   return (
     <Styles.Wrapper>
@@ -25,6 +63,7 @@ export const LoginScreen = () => {
           <Input
             prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
             placeholder="CPF"
+            onChange={event => setCpf(event.target.value)}
           />
         </Form.Item>
         <Form.Item label="Senha">
@@ -32,14 +71,12 @@ export const LoginScreen = () => {
             prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
             type="password"
             placeholder="Senha"
+            onChange={event => setPassword(event.target.value)}
           />
         </Form.Item>
         <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-          >
-            Entrar
+          <Button type="primary" htmlType="submit" onClick={handleClick}>
+            Entrare
           </Button>
         </Form.Item>
       </Form>
