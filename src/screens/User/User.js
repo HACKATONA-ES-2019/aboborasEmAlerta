c41e931001970dd5510715753f8c56e02c106d01
Guilherme Piccoli <guuipiccoli@gmail.com>
2019-09-15 01:14:48 -0300

    style tela do usuario

--- src/screens/User/User.js
+++ src/screens/User/User.js
@@ -7,28 +7,28 @@ const { Title, Paragraph, Text } = Typography;
 class User extends React.Component{
     render() {
         return (
-            <Row>
+            <Row style={{ backgroundColor:"#FBFBFB"}}>
                 <Col span={24}>
-                    <Styles.TextTitle>
-                        Descrição do Ocorrido
+                    <Styles.TextTitle style={{fontSize:30, marginTop: 20}}>
+                        Incêndio - PUCRS
                     </Styles.TextTitle>
                 </Col>
 
                 <Form layout="vertical">
-                        <Col span={12}>
-                            <Styles.ButtonFull type="danger">
-                                PRECISO DE AJUDA!
-                            </Styles.ButtonFull>
+                        <Col span={24}>
+                            <Styles.Buttonfinal type="primary" style={{fontSize: 15, backgroundColor:"#C1C1C1", borderWidth:0}}>
+                                    NÃO ESTOU ENVOLVIDO!
+                            </Styles.Buttonfinal>
                         </Col>
-                        <Col span={12}>
-                            <Styles.ButtonFull type="primary">
+                        <Col span={24}>
+                            <Styles.ButtonFull type="primary" icon="check" style={{fontSize: 30, backgroundColor:"#71BF5C"}}>
                                 ESTOU BEM!
                             </Styles.ButtonFull>
                         </Col>
                         <Col span={24}>
-                            <Styles.Buttonfinal>
-                                NÃO ESTOU ENVOLVIDO!
-                            </Styles.Buttonfinal>
+                            <Styles.ButtonFull type="danger" icon="warning" style={{fontSize: 30, backgroundColor: "#C2504D"}}>
+                                PRECISO DE AJUDA!
+                            </Styles.ButtonFull>
                         </Col>
 
 
